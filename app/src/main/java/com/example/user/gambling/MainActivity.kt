4a4e16bb441f663d68b33377c4f56a5eb8b7a124
AnSrwn <an.srwn@gmail.com>
2018-09-24 11:45:44 +0300

    Package

--- app/src/main/java/com/example/user/gambling/MainActivity.kt
+++ app/src/main/java/com/example/user/gambling/MainActivity.kt
@@ -11,5 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, diceSingleplayerFragment).commit()
+
     }
 }
