e5bef962efc3cb25034993ef1d7b81034e2ba191
jerem <wiedman0@hm.edu>
2018-09-24 12:58:39 +0200

    Implemented options menu. Not working yet.

--- app/src/main/java/com/example/user/gambling/MainActivity.kt
+++ app/src/main/java/com/example/user/gambling/MainActivity.kt
@@ -2,7 +2,11 @@ package com.example.user.gambling
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.user.gambling.game.DiceMenuFragment
+import android.view.MenuItem
+import com.example.user.gambling.settings.SettingsFragment
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +16,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction().add(R.id.fragmentContainer, diceMenuFragment).commit()
+        supportFragmentManager.beginTransaction().addToBackStack(null).add(R.id.fragmentContainer, diceMenuFragment).commit()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return when (item?.itemId) {
+            R.id.settings -> {
+                val settingsFragment = SettingsFragment()
+                supportFragmentManager.beginTransaction().addToBackStack(null).replace(R.id.fragmentContainer, settingsFragment ).commit()
+                true
+            }
+            R.id.help -> {
+                //TODO no help implemented
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
--- app/src/main/java/com/example/user/gambling/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/user/gambling/settings/SettingsFragment.kt
@@ -1,11 +1,16 @@
 package com.example.user.gambling.settings
 
 import android.os.Bundle
-import android.support.v14.preference.PreferenceFragment
+import android.support.v7.preference.Preference
+import android.support.v7.preference.PreferenceFragmentCompat
+import android.support.v7.preference.PreferenceManager
+import com.example.user.gambling.R
 
-class SettingsFragment : PreferenceFragment(){
+class SettingsFragment : PreferenceFragmentCompat(){
 
     override fun onCreatePreferences(p0: Bundle?, p1: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        addPreferencesFromResource(R.xml.preferences)
+        val exercisesPref: Preference = findPreference("pref_user_key")
+        exercisesPref.summary = PreferenceManager.getDefaultSharedPreferences(activity)?.getString("pref_user_key", "")
     }
 }
