9ee833ad39d98793a57e95405da644a91d02195e
AnSrwn <an.srwn@gmail.com>
2018-09-24 13:00:08 +0300

    Linking the fragments together

--- app/src/main/java/com/example/user/gambling/game/DiceMenuFragment.kt
+++ app/src/main/java/com/example/user/gambling/game/DiceMenuFragment.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.user.gambling.R
 
 class DiceMenuFragment : android.support.v4.app.Fragment() {
 
+    private val diceSingleplayerFragment = DiceSingleplayerFragment()
+    private val diceMultiplayerFragment = DiceMultiplayerFragment()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dice_menu, container, false)
+
+        val view = inflater.inflate(R.layout.fragment_dice_menu, container, false)
+        val buttonStartSingleplayerFragment = view.findViewById(R.id.buttonStartSingleplayerFragment) as Button
+        val buttonStartMultiplayerFragment = view.findViewById(R.id.buttonStartMultiplayerFragment) as Button
+
+        buttonStartSingleplayerFragment.setOnClickListener {
+            fragmentManager!!.beginTransaction().replace(R.id.fragmentContainer, diceSingleplayerFragment).addToBackStack(null).commit()
+        }
+
+        buttonStartMultiplayerFragment.setOnClickListener {
+            fragmentManager!!.beginTransaction().replace(R.id.fragmentContainer, diceMultiplayerFragment).addToBackStack(null).commit()
+        }
+
+        return view
     }
 }
