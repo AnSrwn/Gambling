319cb97d6dbc2438ff639cd8cf2d28f746631575
jerem <wiedman0@hm.edu>
2018-09-24 11:09:52 +0200

    Added preference dependencies

---
+++ app/src/main/java/com/example/user/gambling/settings/SettingsFragment.kt
@@ -0,0 +1,11 @@
+package com.example.user.gambling.settings
+
+import android.os.Bundle
+import android.support.v14.preference.PreferenceFragment
+
+class SettingsFragment : PreferenceFragment(){
+
+    override fun onCreatePreferences(p0: Bundle?, p1: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
