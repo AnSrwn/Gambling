2f754d5eaa6889c1aa09e389023da8cab19a16d8
AnSrwn <an.srwn@gmail.com>
2018-10-10 19:23:07 +0300

    comments in MultiplayerFragment

--- app/src/main/java/com/example/user/gambling/game/DiceMultiplayerFragment.kt
+++ app/src/main/java/com/example/user/gambling/game/DiceMultiplayerFragment.kt
@@ -16,6 +16,12 @@ import com.google.android.gms.nearby.Nearby
 import com.google.android.gms.nearby.connection.*
 import java.nio.charset.StandardCharsets.UTF_8
 
+/**
+ * In this fragment, the user connects to another device and is able to play with another user.
+ * For the actual game, gameFragment is used, but the multiplayerFragment is always running (in the
+ * background) to manage the connection.
+ * After one round, the score of the user and the opponent is displayed in the multiplayerFragment.
+ */
 class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
 
     private var connection: Connection? = null
@@ -79,7 +85,11 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         connection!!.connectionsClient.stopAllEndpoints()
     }
 
-    // Callbacks for receiving payloads
+    /**
+     * Callbacks for receiving payloads.
+     * To display the socres, the user must have his/her own score and the opponents score.
+     * Otherwise a waiting screen is displayed until both scores are there.
+     */
     private val payloadCallback = object : PayloadCallback() {
         override fun onPayloadReceived(endpointId: String, payload: Payload) {
             opponentScore = String(payload.asBytes()!!, UTF_8).toInt()
@@ -116,7 +126,12 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         }
     }
 
-    // Callbacks for connections to other devices
+    /**
+     * Callbacks for connections to other devices.
+     * After the connection is established, the username of the opponent is saved,
+     * the search for other devices is stopped and the gameFragment is started.
+     * If the connection is interrupted, the multiplayerFragment is resetted.
+     */
     private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
         override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
             connection!!.connectionsClient.acceptConnection(endpointId, payloadCallback)
@@ -150,6 +165,11 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         }
     }
 
+    /**
+     * The multiplayerFragment is made visible again.
+     * In case, that the user is in the gameFragment at the moment, the gameFragment is removed from
+     * the stack.
+     */
     private fun showMultiplayerFragment() {
         val gameFragment = fragmentManager!!.findFragmentByTag("gameFragment")
         if (gameFragment != null) {
@@ -160,6 +180,11 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         fragmentManager!!.beginTransaction().show(multiplayerFragment!!).commit()
     }
 
+    /**
+     * This method calculates, if the user or the opponent wins this round.
+     * @param myScore score of the user
+     * @param opponentScore score of the opponent
+     */
     private fun setTotalScore(myScore: Int, opponentScore: Int) {
         when {
             myScore > opponentScore -> {
@@ -183,6 +208,9 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         }
     }
 
+    /**
+     * This method finds all views which are needed in this fragment.
+     */
     private fun setAllViews(view: View) {
         yourScoreText = view.findViewById(R.id.textViewMyScore)
         opponentScoreText = view.findViewById(R.id.textViewOpponentScore)
@@ -192,6 +220,10 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         loadingPanel = view.findViewById(R.id.waitForOpponentLoadingPanel)
     }
 
+    /**
+     * This method sets the visibility of the score texts and the continue button.
+     * @param visible if true, the visibilities are set to visible, otherwise to gone.
+     */
     private fun setViewsVisibility(visible: Boolean) {
         if (visible) {
             yourScoreText!!.visibility = View.VISIBLE
@@ -206,6 +238,9 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         }
     }
 
+    /**
+     * This method resets the active multiplayerFragment.
+     */
      private fun resetDiceMultiplayerFragment() {
         findOpponentButton!!.text = getString(R.string.dice_multi_find_opponent)
         findOpponentButton!!.isEnabled = true
@@ -219,6 +254,10 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         opponentTotalScore = 0
     }
 
+    /**
+     * Opens the gameFragment and telling, that it is a Multiplayer game.
+     * Hides the active multiplayerFragment, so that it still runs in the background.
+     */
     private fun startGameFragment() {
         val diceGameFragment = DiceGameFragment()
 
@@ -235,6 +274,9 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         fragmentManager!!.beginTransaction().hide(multiplayerFragment!!).commit()
     }
 
+    /**
+     * Method to get the username.
+     */
     private fun registerForPlayerNameUpdates() {
         activity?.let { fragmentActivity ->
             val sharedViewModel = ViewModelProviders.of(fragmentActivity).get(UserNameViewModel::class.java)
@@ -246,6 +288,9 @@ class DiceMultiplayerFragment : android.support.v4.app.Fragment() {
         }
     }
 
+    /**
+     * Method to get the score, which the user got in the gameFragment.
+     */
     private fun registerForScoreUpdates() {
         activity?.let { fragmentActivity ->
             val sharedViewModel = ViewModelProviders.of(fragmentActivity).get(ScoreViewModel::class.java)
